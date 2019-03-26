b6f9b5909b498ad6405e09d09d33c8c741f5a99c
malnart <tranlamphotography@gmail.com>
2019-03-26 11:32:47 +0700

    .

--- js/main.js
+++ js/main.js
@@ -17,15 +17,6 @@ $(function() {
     };
     navbarFixed();
 
-    $(".hero-banner__video").magnificPopup({
-        disableOn: 700,
-        type: "iframe",
-        mainClass: "mfp-fade",
-        removalDelay: 160,
-        preloader: false,
-        fixedContentPos: false
-    });
-
     if ($('.featured-carousel').length) {
         $('.featured-carousel').owlCarousel({
             loop: false,
