cabc33422c8a2601a1dffc09d62db2935249768b
malnart <tranlamphotography@gmail.com>
2019-03-27 10:19:36 +0700

    .

--- js/main.js
+++ js/main.js
@@ -17,28 +17,6 @@ $(function() {
     };
     navbarFixed();
 
-    if ($('.featured-carousel').length) {
-        $('.featured-carousel').owlCarousel({
-            loop: false,
-            margin: 30,
-            items: 1,
-            nav: true,
-            dots: false,
-            responsiveClass: true,
-            slideSpeed: 300,
-            paginationSpeed: 500,
-            navText : ["<div class='left-arrow'><i class='ti-angle-left'></i></div>","<div class='right-arrow'><i class='ti-angle-right'></i></div>"],
-            responsive: {
-                768: {
-                    items: 2
-                },
-                1100: {
-                    items: 3
-                }
-            }
-        })
-    }
-
     if ($('.hero-carousel').length) {
         $('.hero-carousel').owlCarousel({
             loop: false,
